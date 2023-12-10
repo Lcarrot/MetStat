4ce14ce034b8617ae67c3a6e03638abd13eef57a
katie228 <katehok2010@mail.ru>
2023-12-10 15:21:17 +0500

    send event by click

--- stat-script/script.js
+++ stat-script/script.js
@@ -1,3 +1,6 @@
+const event_array = [];
+const element_array = [];
+
 function addStatWriter() {
     var items = document.getElementsByTagName('body')[0].childNodes;
     for (var i = 0; i < items.length; i++) {
@@ -7,22 +10,42 @@ function addStatWriter() {
 }
 
 function getRecursiveNodes(element) {
-    if (element.childNodes.length === 0) {
-        addClickEvent(element);
-        return;
-    }
+    addClickEvent(element);
     for (var i = 0; i < element.childNodes.length; i++) {
         var child = element.childNodes[i];
-        addClickEvent(element);
         getRecursiveNodes(child);
     }
 }
 
 function addClickEvent(element) {
-    console.log(element);
-    element.addEventListener("click", function(e) {
-        console.log('click');
+    element.addEventListener("click", (e) => {
+        const time = e.timeStamp;
+        if (!(time in event_array)) {
+            addEvent(e, element);
+        } else {
+            const current_val = element_array[time];
+            if (element_array[time].contains(element)) {
+               addEvent(e, element);
+            }
+        }
     });
 }
+
+function addEvent(event, element) {
+    event_array[time] = event;
+    element_array[time] = element;
+}
+
+function sendToServer(event) {
+    console.log('click');
+}
+
+window.addEventListener('beforeunload' ,() => {
+    event_array.forEach((timeStamp, source_event) => {
+        sendToServer(source_event);
+        console.log('time: ' + timeStamp + ' elem: ' +source_event.target);
+    });
+    return true;
+});
 
 addStatWriter();
